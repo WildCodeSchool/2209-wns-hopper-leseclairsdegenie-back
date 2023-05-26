2c6669ec24fc0b34d18c27f9ba252f4f71dd9402
JUSTINE GETS <[justine.gets@posteo.net](mailto:justine.gets@posteo.net)>
2023-05-26 11:48:07 +0200

    add product availability

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,4 +1,4 @@
-import { Field, ID, InputType, ObjectType } from "type-graphql";
+import { Field, FieldResolver, ID, InputType, Int, ObjectType, Root } from "type-graphql";
 import {
   Column,
   Entity,
@@ -8,6 +8,9 @@ import {
 } from "typeorm";
 import { Category } from "./Category";
 import { Reservation } from "./Reservation";
+import datasource from "../utils";
+import { LessThanOrEqual, MoreThanOrEqual } from "typeorm";
+import { Arg } from "type-graphql";
 
 @Entity()
 @ObjectType()
@@ -47,6 +50,39 @@ export class Product {
   @Field(() => Category, { nullable: true })
   @ManyToOne(() => Category, "product", { nullable: true })
   category: Category;
+
+  @Field(() => [ProductAvailability])
+  async availability(
+    @Root() currentProduct: Product,
+    @Arg("startDate", () => Date) startDate: Date,
+    @Arg("endDate", () => Date) endDate: Date,
+    ): Promise<ProductAvailability[]> {
+    const availableDates: ProductAvailability[] = [];
+    const product = await datasource
+        .getRepository(Product)
+        .findOne({ where: { id: currentProduct.id }, relations: { category: true} }); 
+    const lastDate = new Date(startDate.getFullYear(), startDate.getMonth() +1 , 0);
+    for (let date = new Date(startDate); date <= lastDate; date.setDate(date.getDate() + 1)) {
+      let availableQuantity = product.quantity;
+      const startOfDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0,0,0,0);
+      const endOfDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 23,59,59,999);
+      const reservations = 
+      ( 
+        await datasource
+        .getRepository(Reservation)
+        .find({ where: [
+          {product: {id: product.id}, startDate: LessThanOrEqual(startOfDate), endDate: MoreThanOrEqual(startOfDate)},
+          {product: {id: product.id}, startDate: MoreThanOrEqual(startOfDate), endDate: MoreThanOrEqual(endOfDate)},
+          {product: {id: product.id}, startDate: LessThanOrEqual(startOfDate), endDate: MoreThanOrEqual(endOfDate)}
+        ]}) 
+      );
+      for (const reservation of reservations){
+        availableQuantity -= reservation.quantity;
+      }
+      availableDates.push({date: new Date(date), quantity: availableQuantity});
+    }    
+    return availableDates;
+  }
 }
 
 @InputType()
@@ -71,4 +107,13 @@ export class ProductInput {
 
   @Field({ nullable: true })
   categoryId: number;
+}
+
+@ObjectType()
+class ProductAvailability {
+  @Field(() => Date)
+  date: Date;
+
+  @Field(() => Int)
+  quantity: number;  
 }

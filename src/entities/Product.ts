e5f5a683e31e63b82e620b704f2bfca0b864b63a
youcef tellai <ytellai@allohouston.fr>
2023-05-26 16:31:13 +0200

    + mise en place du tunnel de payment

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,74 +1,74 @@
-import { Field, ID, InputType, ObjectType } from "type-graphql";
+import {Field, ID, InputType, ObjectType} from "type-graphql";
 import {
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
+    Column,
+    Entity,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
 } from "typeorm";
-import { Category } from "./Category";
-import { Reservation } from "./Reservation";
+import {Category} from "./Category";
+import {Reservation} from "./Reservation";
 
 @Entity()
 @ObjectType()
 export class Product {
-  @PrimaryGeneratedColumn()
-  @Field(() => ID)
-  id: number;
+    @PrimaryGeneratedColumn()
+    @Field(() => ID)
+    id: number;
 
-  @Column()
-  @Field()
-  name: string;
+    @Column()
+    @Field()
+    name: string;
 
-  @Column({ nullable: true })
-  @Field({ nullable: true })
-  description: string;
+    @Column({nullable: true})
+    @Field({nullable: true})
+    description: string;
 
-  @Column({ nullable: true })
-  @Field({ nullable: true })
-  image: string;
+    @Column({nullable: true})
+    @Field({nullable: true})
+    image: string;
 
-  @Column()
-  @Field()
-  price: number;
+    @Column()
+    @Field()
+    price: number;
 
-  @Column({ nullable: true })
-  @Field({ nullable: true })
-  quantity: number;
+    @Column({nullable: true})
+    @Field({nullable: true})
+    quantity: number;
 
-  @Column({ default: true, nullable: true })
-  @Field({ defaultValue: true, nullable: true })
-  disponibility: boolean;
+    @Column({default: true, nullable: true})
+    @Field({defaultValue: true, nullable: true})
+    disponibility: boolean;
 
-  @Field(() => [Reservation], { nullable: true })
-  @OneToMany(() => Reservation, "product", { nullable: true })
-  reservations: Reservation[];
+    @Field(() => [Reservation], {nullable: true})
+    @OneToMany(() => Reservation, "product", {nullable: true})
+    reservations: Reservation[];
 
-  @Field(() => Category, { nullable: true })
-  @ManyToOne(() => Category, "product", { nullable: true })
-  category: Category;
+    @Field(() => Category, {nullable: true})
+    @ManyToOne(() => Category, "product", {nullable: true})
+    category: Category;
 }
 
 @InputType()
 export class ProductInput {
-  @Field()
-  name: string;
+    @Field()
+    name: string;
 
-  @Field({ nullable: true })
-  description: string;
+    @Field({nullable: true})
+    description: string;
 
-  @Field({ nullable: true })
-  image: string;
+    @Field({nullable: true})
+    image: string;
 
-  @Field()
-  price: number;
+    @Field()
+    price: number;
 
-  @Field({ nullable: true })
-  quantity: number;
+    @Field({nullable: true})
+    quantity: number;
 
-  @Field({ defaultValue: true, nullable: true })
-  disponibility: boolean;
+    @Field({defaultValue: true, nullable: true})
+    disponibility: boolean;
 
-  @Field({ nullable: true })
-  categoryId: number;
+    @Field({nullable: true})
+    categoryId: number;
 }
--- src/resolvers/Products.ts
+++ src/resolvers/Products.ts
@@ -1,118 +1,118 @@
-import { Resolver, Mutation, Arg, Query, ID } from "type-graphql";
-import { Product, ProductInput } from "../entities/Product";
+import {Resolver, Mutation, Arg, Query, ID} from "type-graphql";
+import {Product, ProductInput} from "../entities/Product";
 import datasource from "../utils";
-import { hash } from "argon2";
-import { Category } from "../entities/Category";
+import {hash} from "argon2";
+import {Category} from "../entities/Category";
 
 @Resolver()
 export class ProductsResolver {
-  @Mutation(() => Product)
-  async createProduct(
-    @Arg("data", () => ProductInput) data: ProductInput
-  ): Promise<Product> {
-    console.log(data);
-    const category = await datasource.getRepository(Category).findOneBy({
-      id: data.categoryId,
-    });
-    if (category.id != null) {
-      console.log(category);
-      const product = {
-        ...data,
-        category: category,
-      };
-      return await datasource.getRepository(Product).save(product);
-    }
-  }
-
-  @Query(() => [Product])
-  async products(): Promise<Product[]> {
-    return await datasource
-      .getRepository(Product)
-      .find({ relations: ["category"] });
-  }
-
-  @Mutation(() => Product)
-  async deleteProduct(@Arg("Id", () => ID) id: number): Promise<Product> {
-    let product = await datasource
-      .getRepository(Product)
-      .findOne({ where: { id }, relations: { category: true } });
-    if (product) {
-      await datasource.getRepository(Product).remove(product);
-      return product;
-    } else {
-      return null;
-    }
-  }
-
-  @Query(() => Product)
-  async product(@Arg("Id", () => ID) id: number): Promise<Product> {
-    return await datasource
-      .getRepository(Product)
-      .findOne({ where: { id }, relations: { category: true } });
-  }
-
-  @Mutation(() => Product)
-  async updateProduct(
-    @Arg("Id", () => ID) id: number,
-    @Arg("quantityReserved", () => Number, { nullable: true })
-    quantityReserved: number,
-    @Arg("newPrice", () => Number, { nullable: true }) newPrice: number,
-    @Arg("newQuantity", () => Number, { nullable: true }) newQuantity: number
-  ): Promise<Product> {
-    let product = await datasource
-      .getRepository(Product)
-      .findOne({ where: { id } });
-    let newDisponibility = product.disponibility;
-    let quantityUpdate = product.quantity;
-
-    // Mettre à jour la quantité après un arrivage de nouvelle marchandise
-    if (newQuantity) {
-      if (quantityUpdate) {
-        quantityUpdate += newQuantity;
-      } else {
-        quantityUpdate = newQuantity;
-      }
+    @Mutation(() => Product)
+    async createProduct(
+        @Arg("data", () => ProductInput) data: ProductInput
+    ): Promise<Product> {
+        console.log(data);
+        const category = await datasource.getRepository(Category).findOneBy({
+            id: data.categoryId,
+        });
+        if (category.id != null) {
+            console.log(category);
+            const product = {
+                ...data,
+                category: category,
+            };
+            return await datasource.getRepository(Product).save(product);
+        }
     }
 
-    // mettre à jour les quantité après uen réservation
-    if (quantityReserved) {
-      if (product.quantity - quantityReserved <= 0) {
-        quantityUpdate = 0;
-        newDisponibility = false;
-      } else {
-        quantityUpdate = product.quantity - quantityReserved;
-      }
+    @Query(() => [Product])
+    async products(): Promise<Product[]> {
+        return await datasource
+            .getRepository(Product)
+            .find({relations: ["category"]});
     }
 
-    if (product) {
-      return await datasource.getRepository(Product).save({
-        ...product,
-        quantity: quantityUpdate,
-        disponibility: newDisponibility,
-        price: newPrice ? newPrice : product.price,
-      });
-    } else {
-      return null;
+    @Mutation(() => Product)
+    async deleteProduct(@Arg("Id", () => ID) id: number): Promise<Product> {
+        let product = await datasource
+            .getRepository(Product)
+            .findOne({where: {id}, relations: {category: true}});
+        if (product) {
+            await datasource.getRepository(Product).remove(product);
+            return product;
+        } else {
+            return null;
+        }
     }
-  }
 
-  @Query(() => [Product])
-  async productsRandom(): Promise<Product[]> {
-    const listProducts = await datasource
-      .getRepository(Product)
-      .find({ relations: { category: true } });
-    const max = listProducts.length > 5 ? 5 : listProducts.length;
-
-    let listRandom: Product[] = [];
-
-    for (let i = 0; i < max; i++) {
-      let j = Math.floor(Math.random() * listProducts.length);
-      if (!listRandom?.includes(listProducts[j])) {
-        listRandom.push(listProducts[j]);
-      } else {
-        i--;
-      }
+    @Query(() => Product)
+    async product(@Arg("Id", () => ID) id: number): Promise<Product> {
+        return await datasource
+            .getRepository(Product)
+            .findOne({where: {id}, relations: {category: true}});
     }
-    return listRandom;
-  }
+
+    @Mutation(() => Product)
+    async updateProduct(
+        @Arg("Id", () => ID) id: number,
+        @Arg("quantityReserved", () => Number, {nullable: true})
+            quantityReserved: number,
+        @Arg("newPrice", () => Number, {nullable: true}) newPrice: number,
+        @Arg("newQuantity", () => Number, {nullable: true}) newQuantity: number
+    ): Promise<Product> {
+        let product = await datasource
+            .getRepository(Product)
+            .findOne({where: {id}});
+        let newDisponibility = product.disponibility;
+        let quantityUpdate = product.quantity;
+
+        // Mettre à jour la quantité après un arrivage de nouvelle marchandise
+        if (newQuantity) {
+            if (quantityUpdate) {
+                quantityUpdate += newQuantity;
+            } else {
+                quantityUpdate = newQuantity;
+            }
+        }
+
+        // mettre à jour les quantité après uen réservation
+        if (quantityReserved) {
+            if (product.quantity - quantityReserved <= 0) {
+                quantityUpdate = 0;
+                newDisponibility = false;
+            } else {
+                quantityUpdate = product.quantity - quantityReserved;
+            }
+        }
+
+        if (product) {
+            return await datasource.getRepository(Product).save({
+                ...product,
+                quantity: quantityUpdate,
+                disponibility: newDisponibility,
+                price: newPrice ? newPrice : product.price,
+            });
+        } else {
+            return null;
+        }
+    }
+
+    @Query(() => [Product])
+    async productsRandom(): Promise<Product[]> {
+        const listProducts = await datasource
+            .getRepository(Product)
+            .find({relations: {category: true}});
+        const max = listProducts.length > 5 ? 5 : listProducts.length;
+
+        let listRandom: Product[] = [];
+
+        for (let i = 0; i < max; i++) {
+            let j = Math.floor(Math.random() * listProducts.length);
+            if (!listRandom?.includes(listProducts[j])) {
+                listRandom.push(listProducts[j]);
+            } else {
+                i--;
+            }
+        }
+        return listRandom;
+    }
 }
